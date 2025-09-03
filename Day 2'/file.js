0e569a7ecf16682e9d562c641374bfa084f6162e
aakashk7092 <aakashk7092@gmail.com>
2025-09-03 17:12:44 +0530

    sycronous

--- Day 2'/file.js
+++ Day 2'/file.js
@@ -4,10 +4,5 @@ const fs = require("fs");
 fs.writeFileSync("./test.txt", "Hey there");
 
 // Read from contact.txt
-fs.readFile("./contact.txt", "utf-8", (err, result) => {
-  if (err) {
-    console.log("Error:", err);
-  } else {
-    console.log(result);
-  }
-});
+const result=fs.readFileSync("./contact.txt", "utf-8");
+console.log(result);
